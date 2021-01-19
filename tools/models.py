f4e30a313a5ebfd925fab41ace40b0b57ba62da4
Jiin Park <demon0315@naver.com>
2021-01-19 17:28:20 +0900

    Tool model

--- tools/models.py
+++ tools/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Tool(models.Model):
+    name = models.CharField(max_length=50)
+    kind = models.CharField(max_length=50)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = _("Tool")
+        verbose_name_plural = _("Tools")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Tool_detail", kwargs={"pk": self.pk})
