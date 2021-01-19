becd1434d9e3e9f8227332bc3309ae06741ee08b
Jiin Park <demon0315@naver.com>
2021-01-19 17:30:23 +0900

    Tool admin

--- tools/admin.py
+++ tools/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Tool)
+class ToolAdmin(admin.ModelAdmin):
+    list_display = ("name", "kind")
