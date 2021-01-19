837884cb1123dc1c61442a634b3cdabc83a5e4eb
Jiin Park <demon0315@naver.com>
2021-01-19 17:28:10 +0900

    Idea model

--- ideas/models.py
+++ ideas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Idea(models.Model):
+
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to=None)
+    content = models.TextField()
+    interest = models.IntegerField(default=0)
+    devtool = models.ForeignKey(
+        "tools.Tool", verbose_name="ideas", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = _("Idea")
+        verbose_name_plural = _("Ideas")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("Idea_detail", kwargs={"pk": self.pk})
